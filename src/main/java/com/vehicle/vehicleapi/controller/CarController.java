5defb3af9767afc8f0c7ba215f06e63968d670bf
Naperality <napolytan@gmail.com>
2026-05-25 22:56:18 +0800

    API response format

--- src/main/java/com/vehicle/vehicleapi/controller/CarController.java
+++ src/main/java/com/vehicle/vehicleapi/controller/CarController.java
@@ -2,6 +2,9 @@ package com.vehicle.vehicleapi.controller;
 
 import com.vehicle.vehicleapi.service.CarService;
 import com.vehicle.vehicleapi.model.Car;
+import com.vehicle.vehicleapi.dto.CreateCarRequest;
+import com.vehicle.vehicleapi.dto.UpdateCarRequest;
+import com.vehicle.vehicleapi.dto.ApiResponse;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -21,48 +24,88 @@ public class CarController {
 
     // get all cars in /cars
     @GetMapping
-    public ResponseEntity<List<Car>> getAllCars(){
-        return ResponseEntity.ok(service.getAllCars());
+    public ResponseEntity<ApiResponse<List<Car>>> getAllCars(){
+        ApiResponse<List<Car>> response =
+                new ApiResponse<>(
+                        true,
+                        "Vehicles retrieved successfully",
+                        service.getAllCars()
+                );
+
+        return ResponseEntity.ok(response);
     }
 
     //get specified car in /cars/{ticket}
     @GetMapping("/{ticket}")
-    public ResponseEntity<Car> getCar(@PathVariable int ticket){
+    public ResponseEntity<ApiResponse<Car>> getCar(@PathVariable int ticket){
         Car car = service.getCarByTicket(ticket);
-        if (car == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(car);
+        // if (car == null){
+        //     return ResponseEntity.notFound().build();
+        // }
+        // return ResponseEntity.ok(car); -- useless if since there is already global exception handler
+        ApiResponse<Car> response =
+                new ApiResponse<>(
+                        true,
+                        "Vehicle retrieved successfully",
+                        car
+                );
+        return ResponseEntity.ok(response);
     }
 
     // post to /cars or add
     @PostMapping
-    public ResponseEntity<String> addCar(@Valid@RequestBody Car car){
+    public ResponseEntity<ApiResponse<Void>> addCar(@Valid@RequestBody CreateCarRequest car){
         service.addCar(car);
-        return ResponseEntity.ok("Vehicle Added Successfully!");
+        ApiResponse<Void> response =
+                new ApiResponse<>(
+                        true,
+                        "Vehicle Added Successfully!",
+                        null
+                );
+
+        return ResponseEntity.ok(response);
     }
 
     // deletes car based on ticket in /cars/{ticket}
     @DeleteMapping("/{ticket}")
-    public ResponseEntity<String> deleteCar(@PathVariable int ticket){
-        boolean deleted = service.deleteCar(ticket);
-        if (deleted){
-            return ResponseEntity.ok("Vehicle Deleted Successfully!");
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ApiResponse<Void>> deleteCar(@PathVariable int ticket){
+        // boolean deleted = service.deleteCar(ticket);
+        // if (deleted){
+        //     return ResponseEntity.ok("Vehicle Deleted Successfully!");
+        // }
+        // return ResponseEntity.notFound().build(); -- now useless due to throw exception
+        service.deleteCar(ticket);
+         ApiResponse<Void> response =
+                new ApiResponse<>(
+                        true,
+                        "Vehicle Deleted Successfully!",
+                        null
+                );
+
+        return ResponseEntity.ok(response);
     }
 
     // update using put
-    @PutMapping("/{ticket}")
-    public ResponseEntity<String> updateCar(
+    @PutMapping("/{ticket}")// can use PatchMapping("/{ticket}") for semantically correct
+    public ResponseEntity<ApiResponse<Void>> updateCar(
         @PathVariable int ticket,
-        @Valid@RequestBody Car updatedCar
+        @Valid@RequestBody UpdateCarRequest updatedCar
     ){
-        boolean updated = service.updateCar(ticket, updatedCar);
+        // boolean updated = service.updateCar(ticket, updatedCar);
 
-        if(updated){
-            return ResponseEntity.ok("Vehicle Updated!");
-        }
-        return ResponseEntity.notFound().build();
+        // if(updated){
+        //     return ResponseEntity.ok("Vehicle Updated!");
+        // }
+        // return ResponseEntity.notFound().build(); -- the throw exception is on effect
+        service.updateCar(ticket, updatedCar);
+
+        ApiResponse<Void> response =
+                new ApiResponse<>(
+                        true,
+                        "Vehicle Updated Successfully!",
+                        null
+                );
+
+        return ResponseEntity.ok(response);
     }
 }
