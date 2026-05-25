0187e22f5b8fca828d0e2081d36aff7f631765f0
Naperality <napolytan@gmail.com>
2026-05-25 22:54:25 +0800

    update: repository using PostgreSQL

--- src/main/java/com/vehicle/vehicleapi/repository/CarRepository.java
+++ src/main/java/com/vehicle/vehicleapi/repository/CarRepository.java
@@ -3,25 +3,26 @@ package com.vehicle.vehicleapi.repository;
 import com.vehicle.vehicleapi.model.Car;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 
 @Repository
-public class CarRepository {
-    private final List<Car> cars = new ArrayList<>();
+public interface CarRepository extends JpaRepository<Car, Integer>{
+    // private final List<Car> cars = new ArrayList<>();
 
-    public List<Car> findAll(){
-        return cars;
-    }
+    // public List<Car> findAll(){
+    //     return cars;
+    // }
 
-    public void saveCar(Car car){
-        cars.add(car);
-    }
+    // public void saveCar(Car car){
+    //     cars.add(car);
+    // }
 
-    public boolean deleteByTicket(int ticket){
-        return cars.removeIf(car -> car.getTicket()==ticket);
-    }
+    // public boolean deleteByTicket(int ticket){
+    //     return cars.removeIf(car -> car.getTicket()==ticket);
+    // }
 
 }
