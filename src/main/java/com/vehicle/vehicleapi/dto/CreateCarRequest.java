19a763875a4f0c11e749206a32061cc5221622e7
Naperality <napolytan@gmail.com>
2026-05-25 22:51:47 +0800

    add: DTO or Data Transfer Objects

---
+++ src/main/java/com/vehicle/vehicleapi/dto/CreateCarRequest.java
@@ -0,0 +1,48 @@
+package com.vehicle.vehicleapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CreateCarRequest {
+    @NotBlank(message = "License Plate is Required!")
+    private String licensePlate;
+    @NotBlank(message = "Brand is Required!")
+    private String brand;
+    @NotBlank(message = "Model is Required!")
+    private String model;
+    @NotBlank(message = "Color is Required!")
+    private String color;
+    @NotBlank(message = "Fuel Type is Required!")
+    private String fuelType;
+
+    public CreateCarRequest() {}
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getFuelType() {
+        return fuelType;
+    }
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+}
---
+++ src/main/java/com/vehicle/vehicleapi/dto/UpdateCarRequest.java
@@ -0,0 +1,44 @@
+package com.vehicle.vehicleapi.dto;
+
+public class UpdateCarRequest {
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private String color;
+    private String fuelType;
+
+    public UpdateCarRequest() {}// empty contructor for JSON Deserialization
+
+    // setters and getters
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getFuelType() {
+        return fuelType;
+    }
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+    
+}
