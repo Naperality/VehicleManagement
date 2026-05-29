566c93c951244015a61559d8e553cbaf51429eac
Naperality <napolytan@gmail.com>
2026-05-30 00:26:20 +0800

    update: Logged in User Endpoints

--- src/main/java/com/vehicle/vehicleapi/controller/UserController.java
+++ src/main/java/com/vehicle/vehicleapi/controller/UserController.java
@@ -4,8 +4,10 @@ import java.net.URI;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 // import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vehicle.vehicleapi.model.Car;
 import com.vehicle.vehicleapi.dto.ApiResponse;
 import com.vehicle.vehicleapi.dto.CarResponse;
+import com.vehicle.vehicleapi.dto.ChangePasswordRequest;
 import com.vehicle.vehicleapi.dto.CreateCarRequest;
 import com.vehicle.vehicleapi.dto.CreateUserRequest;
 import com.vehicle.vehicleapi.dto.UpdateCarRequest;
@@ -54,6 +57,7 @@ public class UserController {
     }
 
     // Endpoint for adding users
+    // @PreAuthorize("hasRole('ADMIN')") -- allow to public for registrations
     @Operation(summary = "Creates New User", description = "Adding/Registering more users")
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> addUser(
@@ -73,6 +77,7 @@ public class UserController {
     }
 
     // Endpoint for getting all users
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Read All Users Summary Data", 
         description = "Get all the users information except for password"
@@ -94,6 +99,7 @@ public class UserController {
     }
 
     // endpoint for getting one user by id
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Read One User Data by ID", 
         description = "Get One user information except for password via User ID"
@@ -118,6 +124,7 @@ public class UserController {
     }
 
     // delete user and its owned cars
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Deletes One User Data", 
         description = "Delete all the users information and Connected or Related Cars"
@@ -140,6 +147,7 @@ public class UserController {
     }
 
     // endpoint for updating user info
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Updates One User Data", 
         description = "Update or Edit One/All the users information except for password"
@@ -166,6 +174,7 @@ public class UserController {
     }
 
     // endpoint for user owned cars
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Read One User Connected Cars", 
         description = "Get all the Cars or Vehicles Connected to User"
@@ -175,7 +184,11 @@ public class UserController {
         @Parameter(
             description = "User ID"
         )
-        @PathVariable Long id, Pageable pageable
+        @PathVariable Long id, 
+        @Parameter(
+            description = "Pageable Body"
+        )
+        Pageable pageable
     ){
         Page<Car> userCars = service.getUserCar(id, pageable);
         Page<CarResponse> responses = userCars.map(carMapper::toResponse);
@@ -189,6 +202,7 @@ public class UserController {
     }
 
     // setting cars to user
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Create Car for User", 
         description = "Adding or Registering Cars to User"
@@ -216,6 +230,7 @@ public class UserController {
     }
 
     // updating endpoint for car in user
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
         summary = "Updates One Car on User", 
         description = "Update or Edit One/All the cars information"
@@ -240,6 +255,159 @@ public class UserController {
             new ApiResponse<>(
                 true,
                 "Vehicle Updated!",
+                null
+            )
+        );
+    }
+
+    // Endpoints for users and current users
+    // endpoint for getting one user by id
+    @Operation(
+        summary = "Read Current User Data", 
+        description = "Get current user information except for password"
+    )
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserResponse>> getCurrentUser(){
+        UserResponse response = userMapper.toResponse(
+            service.getCurrentUser()
+        );
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Current User Retrieved",
+                response
+            )
+        );
+    }
+
+    // endpoint for delete own account
+    @Operation(
+        summary = "Deletes Current Account",
+        description = "Deletes current user account along its cars"
+    )
+    @DeleteMapping("/me")
+    public ResponseEntity<ApiResponse<Void>> deleteCurrentUser(){
+        service.deleteUser(service.getCurrentUser().getId());
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Current User Deleted!",
+                null
+            )
+        );
+    }
+
+    // endpoint for update current user details
+    @Operation(
+        summary = "Update Current User Details",
+        description = "Updates/Edit Current logged in user except password"
+    )
+    @PatchMapping("/me")
+    public ResponseEntity<ApiResponse<Void>> updateCurrentUser(
+        @Parameter(
+            description = "Current User Updated Details"
+        )
+        @Valid@RequestBody UpdateUserRequest request
+    ){
+        service.updateCurrentUser(request); 
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Profile Updated Successfully!",
+                null
+            )
+        );
+    }
+
+    // endpoint for getting all cars of current user
+    @Operation(
+        summary = "Read Current User Car",
+        description = "Get all cars connected to current or logged in user"
+    )
+    @GetMapping("/me/cars")
+    public ResponseEntity<ApiResponse<Page<CarResponse>>> getCurrentUserCar(
+        @Parameter(
+            description = "Pageable Body"
+        )
+        Pageable pageable
+    ){
+        Page<Car> cars = service.getCurrentUserCar(pageable);
+        Page<CarResponse> responses = cars.map(carMapper::toResponse);
+
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Cars retrieved",
+                responses
+            )
+        );
+    }
+
+    // adding or creating new cars to current user
+    @Operation(
+        summary = "Create New Cars to Current User",
+        description = "Add or Register More cars to current logged in user"
+    )
+    @PostMapping("/me/cars")
+    public ResponseEntity<ApiResponse<Void>> addCurrentUserCar(
+        @Parameter(
+            description = "Car Body Information"
+        )
+        @Valid @RequestBody CreateCarRequest request
+    ){
+        service.addCurrentUserCar(request);
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Added Vehicle Successfully!",
+                null
+            )
+        );
+    }
+
+    // update specific car of current user endpoint
+    @Operation(
+        summary = "Update Specific Car of Current User",
+        description = "Update/Edit Specific Car of current or logged in user via Car Ticket"
+    )
+    @PatchMapping("/me/cars/{ticket}")
+    public ResponseEntity<ApiResponse<Void>> updateCurrentUserCar(
+        @Parameter(
+            description = "Vehicle ID or Ticket"
+        )
+        @PathVariable Long ticket,
+        @Parameter(
+            description = "Vehicle Body Updates"
+        )
+        @Valid @RequestBody UpdateCarRequest request
+    ){
+        service.updateCurrentUserCar(ticket, request);
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Update Car Successfully!",
+                null
+            )
+        );
+    }
+
+    // update password for current user endpoint
+    @Operation(
+        summary = "Update Password Current User",
+        description = "Update/Edit Password for current logged in user"
+    )
+    @PatchMapping("/me/password")
+    public ResponseEntity<ApiResponse<Void>> updateCurrentUserPassword(
+        @Parameter(
+            description = "Current User new and old passwords"
+        )
+        @Valid @RequestBody ChangePasswordRequest request
+    ){
+        service.updateCurrentUserPassword(request);
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                true,
+                "Change Password Successfully!",
                 null
             )
         );
