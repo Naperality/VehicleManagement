693588a9c1bc492b81e7585b156a2593c8ad601d
Naperality <napolytan@gmail.com>
2026-05-30 00:23:22 +0800

    update: add more service for logged in users

--- src/main/java/com/vehicle/vehicleapi/service/UserService.java
+++ src/main/java/com/vehicle/vehicleapi/service/UserService.java
@@ -1,9 +1,16 @@
 package com.vehicle.vehicleapi.service;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import com.vehicle.vehicleapi.dto.ChangePasswordRequest;
 import com.vehicle.vehicleapi.dto.CreateCarRequest;
 import com.vehicle.vehicleapi.dto.CreateUserRequest;
 import com.vehicle.vehicleapi.dto.UpdateCarRequest;
@@ -11,22 +18,32 @@ import com.vehicle.vehicleapi.dto.UpdateUserRequest;
 import com.vehicle.vehicleapi.dto.UserSummaryResponse;
 import com.vehicle.vehicleapi.exception.CarAlreadyExistException;
 import com.vehicle.vehicleapi.exception.CarNotFoundException;
+import com.vehicle.vehicleapi.exception.RoleOutOfChoiceException;
 import com.vehicle.vehicleapi.exception.UserAlreadyExistException;
 import com.vehicle.vehicleapi.exception.UserNotFoundException;
 import com.vehicle.vehicleapi.model.User;
 import com.vehicle.vehicleapi.model.Car;
+import com.vehicle.vehicleapi.model.Role;
 import com.vehicle.vehicleapi.repository.CarRepository;
 import com.vehicle.vehicleapi.repository.UserRepository;
 
 @Service
 public class UserService {
+    // injection to service
     private final UserRepository repository;
     private final CarRepository carRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository, CarRepository carRepository){
+    public UserService(
+        UserRepository repository, 
+        CarRepository carRepository,
+        PasswordEncoder passwordEncoder
+    ){
         this.repository = repository;
         this.carRepository = carRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
     // adding users
     public void createUser(CreateUserRequest request){
         String email = request.getEmail().toLowerCase();
@@ -43,10 +60,28 @@ public class UserService {
                 "Username already taken!"
             );
         }
+        Set<Role> allowedRoles =
+            Set.of(
+                Role.USER,
+                Role.MECHANIC,
+                Role.GUIDE
+            );
+
+        if(!allowedRoles.contains(
+            request.getRole()
+        )){
+            throw new RoleOutOfChoiceException(
+                "Invalid role"
+            );
+        }
 
         User user = new User();
         user.setEmail(email);
-        user.setPassword(request.getPassword());
+        user.setPassword(
+            passwordEncoder.encode(
+                request.getPassword()
+            )
+        );
         user.setUsername(username);
         user.setRole(request.getRole());
 
@@ -95,6 +130,21 @@ public class UserService {
 
             throw new UserAlreadyExistException(
                 "Username already taken!"
+            );
+        }
+
+        Set<Role> allowedRoles =
+            Set.of(
+                Role.USER,
+                Role.MECHANIC,
+                Role.GUIDE
+            );
+
+        if(request.getRole() != null && !allowedRoles.contains(
+            request.getRole()
+        )){
+            throw new IllegalArgumentException(
+                "Invalid role"
             );
         }
         // boolean alreadyExist = repository.findByEmail(request.getEmail().toLowerCase()).isPresent() || 
@@ -192,5 +242,80 @@ public class UserService {
             car.setModel(request.getModel());
         }
         carRepository.save(car);
+    }
+
+    // get current user
+    public User getCurrentUser(){
+        Authentication authentication = 
+            SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        String username = authentication.getName();
+
+        return repository
+            .findByUsername(username)
+            .orElseThrow(
+                () -> new UserNotFoundException(
+                    "User not Found"
+                )
+            );
+    }
+
+    // update details of current user
+    public void updateCurrentUser(UpdateUserRequest request){
+        User user = getCurrentUser();
+        updateUser(user.getId(), request);
+    }
+
+    // read all cars owned by current user
+    public Page<Car> getCurrentUserCar(Pageable pageable){
+        User user = getCurrentUser();
+        Page<Car> userCars = carRepository.findByUserId(user.getId(), pageable);
+        if(userCars.isEmpty()){
+            throw new CarNotFoundException(
+                "User does not have cars yet!"
+            );
+        }
+        return userCars;
+    }
+
+    // put new car of current user
+    public void addCurrentUserCar(CreateCarRequest request){
+        User user = getCurrentUser();
+        addUserCar(user.getId(), request);
+    }
+
+    // update old car of current user
+    public void updateCurrentUserCar(Long ticket, UpdateCarRequest request){
+        User user = getCurrentUser();
+        updateUserCar(user.getId(), ticket, request);
+    }
+
+    // update password for current logged in user
+    public void updateCurrentUserPassword(ChangePasswordRequest request){
+        User user = getCurrentUser();
+        if(!passwordEncoder.matches(
+            request.getOldPassword(), 
+            user.getPassword())){
+                throw new IllegalArgumentException(
+                    "Current Password is Incorrect!"
+                );
+            }
+        
+        if(passwordEncoder.matches(
+            request.getNewPassword(), 
+            user.getPassword())){
+                throw new IllegalArgumentException(
+                    "New Password is the same to old password!"
+                );
+            }
+        user.setPassword(
+            passwordEncoder.encode(
+                request.getNewPassword()
+            )
+        );
+
+        repository.save(user);
     }
 }
