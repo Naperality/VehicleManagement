d3c1109f56b8a1ca0386d0fa7d4e1705bfcad8c6
Naperality <napolytan@gmail.com>
2026-06-01 17:30:51 +0800

    update: application properties for secret and passwords

--- src/main/java/com/vehicle/vehicleapi/controller/AdminControlller.java
+++ src/main/java/com/vehicle/vehicleapi/controller/AdminControlller.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.web.SecurityMarker;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -39,7 +38,6 @@ import jakarta.validation.Valid;
             Operations Related to Admin and its operations
             """
 )
-
 @SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/admin/users")
 @RestController
