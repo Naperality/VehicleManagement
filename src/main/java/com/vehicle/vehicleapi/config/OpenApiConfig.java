f79c740cb7090a39b67f14ecfe6552f2735600f5
Naperality <napolytan@gmail.com>
2026-06-02 16:58:36 +0800

    add: Open Api Configuration

---
+++ src/main/java/com/vehicle/vehicleapi/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.vehicle.vehicleapi.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+
+        return new OpenAPI()
+            .info(
+                new Info()
+                    .title("Vehicle Management API")
+                    .version("1.0")
+                    .description(
+                        "Vehicle and User Management System"
+                    )
+            );
+    }
+}
