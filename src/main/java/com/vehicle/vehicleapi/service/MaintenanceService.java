7d8a8026084ef9f2a827f30f5ff13dc3dede16af
Naperality <napolytan@gmail.com>
2026-06-04 16:26:31 +0800

    add: Maintenance Service Code

---
+++ src/main/java/com/vehicle/vehicleapi/service/MaintenanceService.java
@@ -0,0 +1,94 @@
+package com.vehicle.vehicleapi.service;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+
+import com.vehicle.vehicleapi.dto.CreateMaintenanceRequest;
+import com.vehicle.vehicleapi.exception.CarNotFoundException;
+import com.vehicle.vehicleapi.exception.UnauthorizedAccessException;
+import com.vehicle.vehicleapi.model.Car;
+import com.vehicle.vehicleapi.model.MaintenanceRecord;
+import com.vehicle.vehicleapi.model.User;
+import com.vehicle.vehicleapi.repository.CarRepository;
+import com.vehicle.vehicleapi.repository.RecordRepository;
+
+@Service
+public class MaintenanceService {
+
+    private final RecordRepository repository;
+    private final CarRepository carRepository;
+    private final UserService userService;
+
+    public MaintenanceService(
+        RecordRepository repository,
+        CarRepository carRepository,
+        UserService userService
+    ){
+        this.repository = repository;
+        this.carRepository = carRepository;
+        this.userService = userService;
+    }
+
+    // adding record 
+    public void createRecord(
+        Long id, 
+        CreateMaintenanceRequest request
+    ){
+        Car car = carRepository
+                .findById(id)
+                .orElseThrow(
+                    () -> new CarNotFoundException(
+                        "Vehicle Not Found!"
+                    )
+                );
+        User user = userService.getCurrentUser();
+        if(!car.getUser().getId().equals(user.getId())){
+            throw new UnauthorizedAccessException(
+                "Vehicle does not belong to user!"
+            );
+        }
+
+        if(
+            request.getMaintenanceDate()
+                .isAfter(LocalDate.now())
+        ){
+            throw new IllegalArgumentException(
+                "Maintenance date cannot be in the future"
+            );
+        }
+
+        MaintenanceRecord record = new MaintenanceRecord();
+
+        record.setServiceType(
+            request.getServiceType()
+        );
+
+        record.setDescription(
+            request.getDescription()
+        );
+
+        record.setMaintenanceDate(
+            request.getMaintenanceDate()
+        );
+
+        record.setMileage(
+            request.getMileage()
+        );
+
+        record.setCost(
+            request.getCost()
+        );
+
+        record.setStatus(
+            request.getStatus()
+        );
+
+        record.setCar(car);
+
+        repository.save(record);
+    }
+
+    // get records of current car
+    
+}
