917d23e3abfc6bd0893a07488e19311da0f449bb
Naperality <napolytan@gmail.com>
2026-06-05 15:49:55 +0800

    update: added find by car ticket

--- src/main/java/com/vehicle/vehicleapi/repository/RecordRepository.java
+++ src/main/java/com/vehicle/vehicleapi/repository/RecordRepository.java
@@ -1,9 +1,11 @@
 package com.vehicle.vehicleapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vehicle.vehicleapi.model.MaintenanceRecord;
 
 public interface RecordRepository extends JpaRepository<MaintenanceRecord, Long>{
-    
+    Page<MaintenanceRecord> findByCarTicket(Long ticket, Pageable pageable);
 }
