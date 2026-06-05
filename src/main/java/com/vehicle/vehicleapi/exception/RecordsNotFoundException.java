15b2fc8ca12e4444b7ad39f986958b3b7db2ab7e
Naperality <napolytan@gmail.com>
2026-06-05 15:49:16 +0800

    add: Record Not Found Exception

---
+++ src/main/java/com/vehicle/vehicleapi/exception/RecordsNotFoundException.java
@@ -0,0 +1,9 @@
+package com.vehicle.vehicleapi.exception;
+
+public class RecordsNotFoundException extends RuntimeException{
+
+    public RecordsNotFoundException(String message){
+        super(message);
+    }
+    
+}
