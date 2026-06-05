095d9981335818fda580742bfd47609dcc76f32f
Naperality <napolytan@gmail.com>
2026-06-05 15:50:34 +0800

    update: get current user records of car

--- src/main/java/com/vehicle/vehicleapi/service/MaintenanceService.java
+++ src/main/java/com/vehicle/vehicleapi/service/MaintenanceService.java
@@ -2,11 +2,16 @@ package com.vehicle.vehicleapi.service;
 
 import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.vehicle.vehicleapi.dto.CreateMaintenanceRequest;
+import com.vehicle.vehicleapi.dto.MaintenanceResponse;
 import com.vehicle.vehicleapi.exception.CarNotFoundException;
+import com.vehicle.vehicleapi.exception.RecordsNotFoundException;
 import com.vehicle.vehicleapi.exception.UnauthorizedAccessException;
+import com.vehicle.vehicleapi.mapper.MaintenanceMapper;
 import com.vehicle.vehicleapi.model.Car;
 import com.vehicle.vehicleapi.model.MaintenanceRecord;
 import com.vehicle.vehicleapi.model.User;
@@ -19,15 +24,18 @@ public class MaintenanceService {
     private final RecordRepository repository;
     private final CarRepository carRepository;
     private final UserService userService;
+    private final MaintenanceMapper mapper;
 
     public MaintenanceService(
         RecordRepository repository,
         CarRepository carRepository,
-        UserService userService
+        UserService userService,
+        MaintenanceMapper mapper
     ){
         this.repository = repository;
         this.carRepository = carRepository;
         this.userService = userService;
+        this.mapper = mapper;
     }
 
     // adding record 
@@ -90,5 +98,33 @@ public class MaintenanceService {
     }
 
     // get records of current car
+    public Page<MaintenanceResponse> getCarRecords(
+        Long id,
+        Pageable pageable
+    ){
+        Car car = carRepository
+                .findById(id)
+                .orElseThrow(
+                    () -> new CarNotFoundException(
+                        "Vehicle Not Found!"
+                    )
+                );
+        User user = userService.getCurrentUser();
+        if(!car.getUser().getId().equals(user.getId())){
+            throw new UnauthorizedAccessException(
+                "Vehicle does not belong to user!"
+            );
+        }
+        Page<MaintenanceRecord> records = repository.findByCarTicket(id, pageable);
+        
+        if(records.isEmpty()){
+            throw new RecordsNotFoundException(
+                "No records found on this car!"
+            );
+        }
+        
+
+        return records.map(mapper::toResponse);
+    }
     
 }
