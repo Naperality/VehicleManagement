7874a95086ebdbb9c95802329011b7fafe3ebba0
Naperality <napolytan@gmail.com>
2026-06-24 23:28:32 +0800

    .

--- src/main/java/com/vehicle/vehicleapi/controller/MaintenanceController.java
+++ src/main/java/com/vehicle/vehicleapi/controller/MaintenanceController.java
@@ -76,6 +76,14 @@ public class MaintenanceController {
         summary = "Current User Records",
         description = "Gets all the current user records"
     )
+
+
+
+
+
+
+
+    
     @GetMapping("/maintenance")
     public ResponseEntity<ApiResponse<Page<MaintenanceResponse>>> getUserRecords(
         @Parameter(
